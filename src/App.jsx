7dc5bff35f394135962090bdbee89114ac4f407b
nizamuddin0172 <neom01723441081@gmail.com>
2024-05-26 10:09:17 +0600

    adding navbar sectiong

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import NavBar from './NavBar/NavBar'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
   
   return (
     <>
-      
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
       <h1 className='text-2xl'>Vite + React</h1>
       
     </>
